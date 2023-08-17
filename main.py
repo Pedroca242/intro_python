872b54afc51a5f1fec53251c6a1e46c064464e2a
Pedro Lucas <pedroca242@gmail.com>
2023-08-16 23:25:48 -0300

    find_path atualizado (a*)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from queue import PriorityQueue
+
 
 class cliente:
     def __init__(self, id, pos, goal):
@@ -12,6 +14,17 @@ class carro:
     def __init__(self, id, pos):
         self.id = id
         self.pos = pos
+
+class Node:
+    def __init__(self, pos, parent=None):
+        self.pos = pos
+        self.parent = parent
+        self.g = 0
+        self.h = manhattan_distance(pos, cliente1.goal)
+        self.f = self.g + self.h
+
+    def __lt__(self, other):  # Adicione este método para fazer a comparação funcionar
+        return self.f < other.f
 
 def draw_map():
     mapa = np.ones((101, 101))
@@ -30,24 +43,38 @@ def update_graph(new_pos):
 def manhattan_distance(pos1, pos2):
     return abs(pos2[0] - pos1[0]) + abs(pos2[1] - pos1[1])
 
-def find_path(start, goal):
-    path = []
-    current_pos = start
-    min_distance_value = manhattan_distance(current_pos, goal)
+def find_path(start, goal, mapa):
+    open_list = PriorityQueue()
+    start_node = Node(start)
+    open_list.put((start_node.f, start_node))
 
-    while manhattan_distance(current_pos, goal):
-        new_pos = [[current_pos[0] - 1, current_pos[1]], [current_pos[0] + 1, current_pos[1]], [current_pos[0], current_pos[1] - 1], [current_pos[0], current_pos[1] + 1]]
+    while not open_list.empty():
+        _, current_node = open_list.get()
+
+        if np.array_equal(current_node.pos, goal):
+            path = []
+            while current_node:
+                path.insert(0, current_node.pos)
+                current_node = current_node.parent
+            return path
+
+        new_pos = [[current_node.pos[0] - 1, current_node.pos[1]],
+                   [current_node.pos[0] + 1, current_node.pos[1]],
+                   [current_node.pos[0], current_node.pos[1] - 1],
+                   [current_node.pos[0], current_node.pos[1] + 1]]
+
         for i in new_pos:
-            if manhattan_distance(i, goal) < min_distance_value:
-                min_distance_value = manhattan_distance(i, goal)
-                path.append(i)
-                current_pos = i
-    return path
+            if 0 <= i[0] < mapa.shape[0] and 0 <= i[1] < mapa.shape[1] and mapa[i[0], i[1]] == 0:
+                new_node = Node(i, parent=current_node)
+                open_list.put((new_node.f, new_node))
+                mapa[i[0], i[1]] = 1  # Mark as visited
+
+    return []
 
 plt.ion()
 
-cliente1 = cliente(321, np.array([80, 80]), np.array([2, 2]))
-carro1 = carro(123, np.array([40, 40]))
+cliente1 = cliente(321, np.array([40, 40]), np.array([2, 2]))
+carro1 = carro(123, np.array([20, 80]))
 
 flag = True
 
@@ -56,11 +83,11 @@ figure, ax, mapa = draw_map()
 poscliente = ax.scatter(cliente1.pos[0], cliente1.pos[1], s = 20, zorder = 2)
 poscarro = ax.scatter(carro1.pos[0], carro1.pos[1], s = 20, zorder = 2)
 
-carro_path = find_path(carro1.pos, cliente1.pos)
+carro_path = find_path(carro1.pos, cliente1.pos, mapa)
 
 while flag:
     for i in carro_path:
+        carro1.pos = np.array(i)
         update_graph(i)
         time.sleep(0.1)
-
 
