d9148fad41d7c28c8233bf2d2d715691874b22fe
Pedro Lucas <pedroca242@gmail.com>
2023-08-17 23:45:55 -0300

    find path corrigido

--- main.py
+++ main.py
@@ -15,17 +15,6 @@ class carro:
         self.id = id
         self.pos = pos
 
-class Node:
-    def __init__(self, pos, parent=None):
-        self.pos = pos
-        self.parent = parent
-        self.g = 0
-        self.h = manhattan_distance(pos, cliente1.goal)
-        self.f = self.g + self.h
-
-    def __lt__(self, other):  # Adicione este método para fazer a comparação funcionar
-        return self.f < other.f
-
 def draw_map():
     mapa = np.ones((101, 101))
     figure, ax = plt.subplots(figsize=(5,5))
@@ -44,37 +33,46 @@ def manhattan_distance(pos1, pos2):
     return abs(pos2[0] - pos1[0]) + abs(pos2[1] - pos1[1])
 
 def find_path(start, goal, mapa):
-    open_list = PriorityQueue()
-    start_node = Node(start)
-    open_list.put((start_node.f, start_node))
+    path = []
+    current_pos = start
+    ruas = [0, 20, 40, 60, 80, 100]
+    flag = [True, True]
 
-    while not open_list.empty():
-        _, current_node = open_list.get()
+    while not np.array_equal(current_pos, goal):
+        if current_pos[0] in ruas and flag[0]:
+            if current_pos[1] < ruas[np.absolute(ruas - goal[1]).argmin()]:
+                current_pos[1] += 1
+            elif current_pos[1] > ruas[np.absolute(ruas - goal[1]).argmin()]:
+                current_pos[1] -= 1
+            else:
+                while current_pos[0] != goal[0]:
+                    if current_pos[0] < goal[0]:
+                        current_pos[0] += 1
+                    elif current_pos[0] > goal[0]:
+                        current_pos[0] -= 1
+                    path.append([current_pos[0], current_pos[1]])
+                flag[0] = False
+        elif current_pos[1] in ruas and flag[1]:
+            if current_pos[0] < ruas[np.absolute(ruas - goal[0]).argmin()]:
+                current_pos[0] += 1
+            elif current_pos[0] > ruas[np.absolute(ruas - goal[0]).argmin()]:
+                current_pos[0] -= 1
+            else:
+                while current_pos[1] != goal[1]:
+                    if current_pos[1] < goal[1]:
+                        current_pos[1] += 1
+                    elif current_pos[1] > goal[1]:
+                        current_pos[1] -= 1
+                    path.append([current_pos[0], current_pos[1]])
+                flag[1] = False
 
-        if np.array_equal(current_node.pos, goal):
-            path = []
-            while current_node:
-                path.insert(0, current_node.pos)
-                current_node = current_node.parent
-            return path
-
-        new_pos = [[current_node.pos[0] - 1, current_node.pos[1]],
-                   [current_node.pos[0] + 1, current_node.pos[1]],
-                   [current_node.pos[0], current_node.pos[1] - 1],
-                   [current_node.pos[0], current_node.pos[1] + 1]]
-
-        for i in new_pos:
-            if 0 <= i[0] < mapa.shape[0] and 0 <= i[1] < mapa.shape[1] and mapa[i[0], i[1]] == 0:
-                new_node = Node(i, parent=current_node)
-                open_list.put((new_node.f, new_node))
-                mapa[i[0], i[1]] = 1  # Mark as visited
-
-    return []
+        path.append([current_pos[0], current_pos[1]])
+    return path
 
 plt.ion()
 
-cliente1 = cliente(321, np.array([40, 40]), np.array([2, 2]))
-carro1 = carro(123, np.array([20, 80]))
+cliente1 = cliente(321, np.array([100, 100]), np.array([2, 2]))
+carro1 = carro(123, np.array([7, 0]))
 
 flag = True
 
