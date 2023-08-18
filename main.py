5d10143d0a198f7bb079f6fd3c76471b6eb4e947
Pedro Lucas <pedroca242@gmail.com>
2023-08-18 00:31:59 -0300

    plot nas classes e posicao aleatoria dos carros

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from queue import PriorityQueue
-
 
 class cliente:
     def __init__(self, id, pos, goal):
@@ -10,10 +8,27 @@ class cliente:
         self.pos = pos
         self.goal = goal
 
+    def create_point(self, ax):
+        self.pos_graph = ax.scatter(self.pos[0], self.pos[1], s=20, zorder=2, c='red')
+        return self.pos_graph
+    def update_graph(self, new_pos, ax):
+        self.pos_graph.set_offsets(np.c_[new_pos[0], new_pos[1]])
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+
 class carro:
     def __init__(self, id, pos):
         self.id = id
         self.pos = pos
+
+    def create_point(self, ax):
+        self.pos_graph = ax.scatter(self.pos[0], self.pos[1], s=20, zorder=2, c='blue')
+        return self.pos_graph
+
+    def update_graph(self, figure, new_pos, ax):
+        self.pos_graph.set_offsets(np.c_[new_pos[0], new_pos[1]])
+        figure.canvas.draw()
+        figure.canvas.flush_events()
 
 def draw_map():
     mapa = np.ones((101, 101))
@@ -23,11 +38,6 @@ def draw_map():
         mapa[:, i] = 0
     ax.imshow(mapa, cmap = 'gray_r', vmin = 0, vmax = 1,origin='lower')
     return figure, ax, mapa
-
-def update_graph(new_pos):
-    poscarro.set_offsets(np.c_[new_pos[0], new_pos[1]])
-    figure.canvas.draw()
-    figure.canvas.flush_events()
 
 def manhattan_distance(pos1, pos2):
     return abs(pos2[0] - pos1[0]) + abs(pos2[1] - pos1[1])
@@ -69,23 +79,40 @@ def find_path(start, goal, mapa):
         path.append([current_pos[0], current_pos[1]])
     return path
 
+def random_pos(matriz):
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+
+    while True:
+        linha_aleatoria = np.random.randint(0, linhas - 1)
+        coluna_aleatoria = np.random.randint(0, colunas - 1)
+
+        if matriz[linha_aleatoria][coluna_aleatoria] == 0:
+            return [linha_aleatoria, coluna_aleatoria]
+
 plt.ion()
 
-cliente1 = cliente(321, np.array([100, 100]), np.array([2, 2]))
-carro1 = carro(123, np.array([7, 0]))
-
+cliente1 = cliente(321, np.array([45, 60]), np.array([2, 2]))
 flag = True
 
 figure, ax, mapa = draw_map()
 
-poscliente = ax.scatter(cliente1.pos[0], cliente1.pos[1], s = 20, zorder = 2)
-poscarro = ax.scatter(carro1.pos[0], carro1.pos[1], s = 20, zorder = 2)
+n_carros = 3
+carros = []
+carros_path = []
 
-carro_path = find_path(carro1.pos, cliente1.pos, mapa)
+for i in range(n_carros):
+    carros.append(carro(np.random.randint(100), random_pos(mapa)))
+
+cliente1.create_point(ax)
+for n, j in enumerate(carros):
+    j.create_point(ax)
+    carros_path.append(find_path(carros[n].pos, cliente1.pos, mapa))
 
 while flag:
-    for i in carro_path:
-        carro1.pos = np.array(i)
-        update_graph(i)
-        time.sleep(0.1)
+    for i, j in zip(carros, carros_path):
+        for k in j:
+            i.pos = np.array(k)
+            i.update_graph(figure, k, ax)
+            time.sleep(0.1)
 
